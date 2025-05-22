c704e08cab93e15f1641c86ddc0fa287b25e3b78
sahoonishi <nishisahoo293@gmail.com>
2025-05-22 23:11:27 +0530

    mobile done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -14,9 +14,11 @@ const Hero = () => {
       </div>
 
       {/* Text container */}
-      <div className="absolute max-w-[80%] md:max-w-[65%] top-52 left-6 md:top-52 md:left-40 text-white text-3xl md:text-5xl
+      <div
+        className="absolute max-w-[80%] md:max-w-[65%] top-52 left-6 md:top-52 md:left-40 text-white text-3xl md:text-5xl
                       sm:top-40 sm:left-4 sm:text-2xl
-                      xs:max-w-[90%] xs:left-2 xs:top-32 xs:text-xl">
+                      xs:max-w-[90%] xs:left-2 xs:top-32 xs:text-xl"
+      >
         <p className="md:leading-16 md:tracking-wide font-light">
           Your Trusted{" "}
           <span className="font-medium">
@@ -36,49 +38,52 @@ const Hero = () => {
 
       {/* Stats bar */}
       <div className="absolute bottom-0 w-full text-white">
-        <div className="bg-black/45 flex items-start py-1 justify-evenly text-xl lg:text-2xl xl:text-3xl px-6
-                        sm:flex-wrap sm:gap-4 sm:justify-evenly sm:text-lg">
-          <div className="flex gap-1 flex-col max-w-[30%] items-center sm:max-w-[45%] xs:max-w-[90%]">
+        <div
+          className="bg-black/45 flex items-start py-1 justify-between text-lg lg:text-2xl xl:text-3xl px-3 md:px-6
+                        flex-wrap sm:gap-4 sm:justify-evenly sm:text-lg"
+        >
+          <div className="flex gap-1 flex-col max-w-[25%] items-center sm:max-w-[45%] xs:max-w-[90%]">
             <img
+            
               src="https://xcelore.com/wp-content/uploads/2025/01/icon.png.webp"
               alt=""
-              className="size-12"
+              className="md:size-12 size-6"
             />
-            <p className="font-[700]">15+</p>
-            <p className="text-sm md:text-xl font-[300] text-center text-gray-400">
+            <p className="md:font-[700] font-[300]">15+</p>
+            <p className="text-xs md:text-xl font-[300] text-center text-gray-400">
               Global Clients
             </p>
           </div>
-          <div className="flex flex-col gap-2 max-w-[30%] items-center sm:max-w-[45%] xs:max-w-[90%]">
+          <div className="flex flex-col gap-2 max-w-[25%] items-center sm:max-w-[45%] xs:max-w-[90%]">
             <img
               src="https://xcelore.com/wp-content/uploads/2025/01/Layer_1.png.webp"
               alt=""
-              className="size-12"
+              className="md:size-12 size-6"
             />
-            <p className="font-[700]">25+</p>
-            <p className="text-sm md:text-xl font-[300] text-center text-gray-400">
+            <p className="md:font-[700] font-[300]">25+</p>
+            <p className="text-xs md:text-xl font-[300] text-center text-gray-400">
               Successful Projects
             </p>
           </div>
-          <div className="flex flex-col gap-2 max-w-[20%] text-center items-center sm:max-w-[45%] xs:max-w-[90%]">
+          <div className="flex flex-col gap-2 max-w-[25%] text-center items-center sm:max-w-[45%] xs:max-w-[90%]">
             <img
               src="https://xcelore.com/wp-content/uploads/2025/01/Layer_x0020_1.png.webp"
               alt=""
-              className="size-12"
+              className="md:size-12 size-6"
             />
-            <p className="font-[700]">50+</p>
-            <p className="text-sm md:text-xl font-[300] text-center text-gray-400">
+            <p className="md:font-[700] font-[300]">50+</p>
+            <p className="text-xs line-clamp-2 md:text-xl font-[300] text-center text-gray-400">
               Years of Combined Leadership experience
             </p>
           </div>
-          <div className="flex flex-col gap-2 max-w-[30%] items-center sm:max-w-[45%] xs:max-w-[90%]">
+          <div className="flex flex-col gap-2 max-w-[25%] items-center sm:max-w-[45%] xs:max-w-[90%]">
             <img
               src="https://xcelore.com/wp-content/uploads/2025/01/Vector4.png.webp"
               alt=""
-              className="size-12"
+              className="md:size-12 size-6"
             />
-            <p className="font-[700]">125+</p>
-            <p className="text-sm md:text-xl text-center font-[300] text-gray-400">
+            <p className="md:font-[700] font-[300]">125+</p>
+            <p className="text-xs md:text-xl text-center font-[300] text-gray-400">
               Team Members
             </p>
           </div>
